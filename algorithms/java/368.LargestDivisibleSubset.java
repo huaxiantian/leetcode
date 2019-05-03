aa5f18744150dfd129733e972985cb0d4a450175
huaxiantian <45733534+huaxiantian@users.noreply.github.com>
2019-05-03 16:26:57 +0800

    Update 368.LargestDivisibleSubset.java

--- algorithms/java/368.LargestDivisibleSubset.java
+++ algorithms/java/368.LargestDivisibleSubset.java
@@ -1,12 +1,18 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
+        //给数组排序并全部赋予初始值1
         Arrays.sort(nums);
+        Arrays.fill(dp, 1);
+        
+        //数组为空，输出空列表
         List<Integer> list = new ArrayList<>();
         if(nums.length == 0) return list;
+        
+        //建立dp方程
         int[] dp = new int[nums.length];
         int[] num = new int[nums.length];
-        Arrays.fill(dp, 1);
         int max = 1, max_last = 0;
+        
         for(int i =1; i < nums.length; i++){
             for(int j = 0; j < i; j++){
                 if(dp[j] + 1 > dp[i] && nums[i] % nums[j] == 0 ){
@@ -19,6 +25,7 @@ class Solution {
                 }
             }
         }
+        
         for(int i = 0; i < max; i++){
             list.add(nums[max_last]);
             max_last = num[max_last];
