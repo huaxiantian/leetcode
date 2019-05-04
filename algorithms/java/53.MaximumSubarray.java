f4d35ae4678ae317ab57ce1f1629a9e8dea318aa
huaxiantian <45733534+huaxiantian@users.noreply.github.com>
2019-05-04 14:31:23 +0800

    Update 53.MaximumSubarray.java

--- algorithms/java/53.MaximumSubarray.java
+++ algorithms/java/53.MaximumSubarray.java
@@ -2,6 +2,7 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE, dp = 0;
         for(int num : nums){
+            //当dp<0时，最大数必定从num[i]开始计数
             dp = Math.max(dp + num, num);
             max = Math.max(dp, max);
         }
