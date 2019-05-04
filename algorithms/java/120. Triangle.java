8fd8ba801b8558f3353c0cb5fece12d29adb425a
huaxiantian <45733534+huaxiantian@users.noreply.github.com>
2019-05-04 15:24:12 +0800

    Create 120. Triangle.java

---
+++ algorithms/java/120. Triangle.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int m = triangle.get(n - 1).size();
+        int[][] dp = new int[n][m];
+        
+        dp[0][0] = triangle.get(0).get(0);
+        
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i-1][0] + triangle.get(i).get(0);
+        }
+        
+        for(int i = 1; i < n; i++){
+            dp[i][i] = dp[i-1][i-1] + triangle.get(i).get(i);
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < triangle.get(i).size() - 1; j++){
+                dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i).get(j);
+            }
+        }
+        
+        int min = dp[n - 1][0];
+        for(int i = 1; i < m; i++){
+            min = dp[n - 1][i] > min ? min : dp[n - 1][i];
+        }
+        return min ;
+    }
+}
