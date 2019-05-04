4136f6c3c7977081000845c94045638f60507e63
huaxiantian <45733534+huaxiantian@users.noreply.github.com>
2019-05-04 20:22:35 +0800

    Update 120.Triangle.java

--- algorithms/java/120.Triangle.java
+++ algorithms/java/120.Triangle.java
@@ -16,7 +16,7 @@ class Solution {
             dp[i][i] = dp[i-1][i-1] + triangle.get(i).get(i);
         }
         
-        //一般情况
+        //一般情况，自顶而下
         for(int i = 1; i < n; i++){
             for(int j = 1; j < triangle.get(i).size() - 1; j++){
                 dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i).get(j);
@@ -29,6 +29,22 @@ class Solution {
             min = dp[n - 1][i] > min ? min : dp[n - 1][i];
         }
         return min ;
+    }
+}
+
+一维数组
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int dp[] = new int[n + 1];
+        
+        //在原数组上改动，自下而上
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
     }
 }
 
