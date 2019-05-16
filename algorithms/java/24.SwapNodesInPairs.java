93431cfdaeec81967232eb4b17c340e50f8fdac9
huaxiantian <45733534+huaxiantian@users.noreply.github.com>
2019-05-16 20:34:49 +0800

    Create 24.SwapNodesInPairs.java

---
+++ algorithms/java/24.SwapNodesInPairs.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode pre = head.next;
+        head.next = swapPairs(head.next.next);
+        pre.next = head;
+        
+        return pre;
+    }
+}
